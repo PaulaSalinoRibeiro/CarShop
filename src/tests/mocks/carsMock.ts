859d1ce2cca8af66aed809b8471f8bb533913811
Paula <paulasalinor@gmail.com>
2022-09-14 17:52:40 -0300

    feat: add mock of cars list

--- src/tests/mocks/carsMock.ts
+++ src/tests/mocks/carsMock.ts
@@ -18,3 +18,22 @@ export const carsObjOutput: ICar & {_id: string} = {
   doorsQty: 2,
   _id: "632231a9c9b779b39ada8047",
 }
+
+export const carsListOutput: ICar[] = [
+  {
+    model: "Ferrari Maranello",
+    year: 1963,
+    color: "red",
+    buyValue: 3500000,
+    seatsQty: 2,
+    doorsQty: 2
+  },
+  {
+    model: "Ferrari Maranello",
+    year: 1963,
+    color: "red",
+    buyValue: 3500000,
+    seatsQty: 2,
+    doorsQty: 2
+  }
+]
