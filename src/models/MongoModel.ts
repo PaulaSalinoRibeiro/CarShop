5e8d881825cf6ad6e43b91ca5ce61d43bd677f4e
Paula <paulasalinor@gmail.com>
2022-09-14 17:02:29 -0300

    fix: constructor recive Model<T>

--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -4,27 +4,29 @@ import { IModel } from '../interfaces/IModel';
 
 export default abstract class MongoModel<T> implements IModel<T> {
   private static message = 'InválidID';
+  protected _model: Model<T>;
 
-  constructor(protected model = Model) {
-    this.model = Model;
+  // Pq a Model do moogose permite passar um generic?
+  constructor(model:Model<T>) {
+    this._model = model;
   }
 
   public async create(obj: T): Promise<T> {
-    return this.model.create(obj);
+    return this._model.create({ ...obj });
   }
 
   public async read(): Promise<T[]> {
-    return this.model.find();
+    return this._model.find();
   }
 
   public async readOne(id: string): Promise<T | null> {
     if (!isValidObjectId(id)) throw new HandleError(400, MongoModel.message);
-    return this.model.findOne({ id });
+    return this._model.findOne({ id });
   }
 
   public async update(id: string, obj: T): Promise<T | null> {
     if (!isValidObjectId(id)) throw new HandleError(400, MongoModel.message);
-    return this.model.findByIdAndUpdate(
+    return this._model.findByIdAndUpdate(
       { id },
       { ...obj } as UpdateQuery<T>,
       { new: true },
@@ -32,6 +34,6 @@ export default abstract class MongoModel<T> implements IModel<T> {
   }
   public async delete(id: string): Promise<T | null> {
     if (!isValidObjectId(id)) throw new HandleError(400, MongoModel.message);
-    return this.model.findByIdAndDelete({ id });
+    return this._model.findByIdAndDelete({ id });
   }
 }
