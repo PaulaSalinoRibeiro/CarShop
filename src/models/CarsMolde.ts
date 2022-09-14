aed5203442760d577d2ba5319f9dec971c201629
Paula <paulasalinor@gmail.com>
2022-09-14 15:57:13 -0300

    feat: add CarsModel with extends MongoModel

---
+++ src/models/CarsMolde.ts
@@ -0,0 +1,18 @@
+import { model as moogoseCreateMode, Schema } from 'mongoose';
+import { ICar } from '../interfaces/ICar';
+import MongoModel from './MongoModel';
+
+const carsMoogoseSchema = new Schema<ICar>({
+  model: String,
+  year: Number,
+  color: String,
+  buyValue: Number,
+  seatsQty: Number,
+  doorsQty: Number,
+});
+
+export default class CarsModel extends MongoModel<ICar> {
+  constructor(model = moogoseCreateMode('Cars', carsMoogoseSchema)) {
+    super(model);
+  }
+}
