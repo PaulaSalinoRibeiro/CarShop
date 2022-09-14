c570526a11df6ca215f61409cabfd8685754c319
Paula <paulasalinor@gmail.com>
2022-09-14 17:51:58 -0300

    feat: add list with returns status 200 and list of cars

--- src/controllers/CarsController.ts
+++ src/controllers/CarsController.ts
@@ -9,6 +9,11 @@ class CarsController {
     const result = await this.service.create(req.body);
     return res.status(201).json(result);
   }
+
+  async list(req: Request, res: Response): Promise<Response> {
+    const result = await this.service.list();
+    return res.status(200).json(result);
+  }
 }
 
 export default CarsController;
