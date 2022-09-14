36b9ae237c87658ff247a8eb7df2de5b0c4adbcc
Paula <paulasalinor@gmail.com>
2022-09-14 19:45:12 -0300

    remove console.log

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -2,11 +2,10 @@ import { ErrorRequestHandler } from 'express';
 
 const errorMiddleware: ErrorRequestHandler = async (err, _req, res, _next) => {
   const { status, message } = err;
-  console.log('passou aqui', err.message);
   if (!status) {
     return res.status(500).json({ message: 'internal error! =/' });
   }
-  return res.status(status).json({ message });
+  return res.status(status).json({ error: message });
 };
 
 export default errorMiddleware;
