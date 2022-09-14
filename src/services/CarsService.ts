b5eedf1ecd5a5f037021f4cf43d31ed0ad2ca501
Paula <paulasalinor@gmail.com>
2022-09-14 17:03:08 -0300

    feat: CarsService with create method

---
+++ src/services/CarsService.ts
@@ -0,0 +1,20 @@
+import IService from '../interfaces/IService';
+import { ICar, carSchema } from '../interfaces/ICar';
+import { IModel } from '../interfaces/IModel';
+import HandleError from '../util/HandleError';
+
+class CarsService implements IService<ICar> {
+  private carModel: IModel<ICar>;
+
+  constructor(model: IModel<ICar>) {
+    this.carModel = model;
+  }
+
+  public async create(obj: ICar) {
+    const parsed = carSchema.safeParse(obj);
+    if (!parsed.success) throw new HandleError(400, 'Invalid fields');
+    return this.carModel.create(obj);
+  }
+}
+
+export default CarsService;
