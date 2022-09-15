3af6812ec732fc06b02b63734101b1e87075a334
Paula <paulasalinor@gmail.com>
2022-09-15 10:29:36 -0300

    test: add motorcycleMock for tests

---
+++ src/tests/mocks/motorcycleMock.ts
@@ -0,0 +1,20 @@
+import { IMotorcycle } from "../../interfaces/IMotorcycle";
+
+export const motorcycleObjInput: IMotorcycle = {
+  model: "Honda CG Titan 125",
+  year: 1963,
+  color: "red",
+  buyValue: 3500,
+  category: "Street",
+  engineCapacity: 125
+}
+
+export const motorcycleObjOutput: IMotorcycle & { _id: string } = {
+  _id: "4edd40c86762e0fb12000003",
+  model: "Honda CG Titan 125",
+  year: 1963,
+  color: "red",
+  buyValue: 3500,
+  category: "Street",
+  engineCapacity: 125
+}
