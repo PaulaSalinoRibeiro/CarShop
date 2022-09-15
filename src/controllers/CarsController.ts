a20040628d3a23488dfd4f41ccc1b8bb2b1dbabe
Paula <paulasalinor@gmail.com>
2022-09-14 21:40:40 -0300

    feat: add delete method in carsController

--- src/controllers/CarsController.ts
+++ src/controllers/CarsController.ts
@@ -25,6 +25,11 @@ class CarsController {
     const result = await this.service.update(req.params.id, req.body);
     return res.status(200).json(result);
   }
+
+  async delete(req: Request, res: Response): Promise<Response | void> {
+    await this.service.delete(req.params.id);
+    return res.status(204).end();
+  }
 }
 
 export default CarsController;
