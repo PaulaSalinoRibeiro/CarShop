69d78d2cfd50f94b0450eea92f185b10b2245d2e
Paula <paulasalinor@gmail.com>
2022-09-14 21:18:39 -0300

    feat: add update

--- src/interfaces/IService.ts
+++ src/interfaces/IService.ts
@@ -2,4 +2,5 @@ export default interface IService<T> {
   create(obj: T): Promise<T>
   list(): Promise<T[]>
   listOne(id: string): Promise<T | null>
+  update(id: string, obj: T): Promise<T | null>
 }
