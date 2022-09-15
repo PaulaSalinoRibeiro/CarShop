13e7721cc3557b78a654c2e917d80ae0a233042a
Paula <paulasalinor@gmail.com>
2022-09-14 21:39:55 -0300

    feat: add delete

--- src/interfaces/IService.ts
+++ src/interfaces/IService.ts
@@ -3,4 +3,5 @@ export default interface IService<T> {
   list(): Promise<T[]>
   listOne(id: string): Promise<T | null>
   update(id: string, obj: T): Promise<T | null>
+  delete(id: string): Promise<void>
 }
